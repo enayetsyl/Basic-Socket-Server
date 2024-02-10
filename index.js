9a441cafc0a7e86578196689366de3271dccdb21
Enayet <enayetflweb@gamil.com>
2024-02-10 08:50:26 +0600

    netlify live site link added to cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://splendorous-parfait-8e5acc.netlify.app"],
     methods: ["GET", "POST"],
   },
 });
